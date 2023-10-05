869dda1d8d3df66c7160fa747480f915e626e7a0
raassh-23 <muh.nur.abdurrauf@gmail.com>
2023-10-05 13:15:11 +0700

    add react client

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 
 const { name, version } = require('../package.json');
+const path = require('path');
+
 const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
 
 const leaderboard = require('./api/leaderboard');
 const LeaderboardServices = require('./services/postgres/LeaderboardServices');
@@ -22,6 +26,11 @@ const init = async () => {
         },
     });
 
+    await server.register([
+        Inert,
+        Vision,
+    ]);
+
     server.route({
         method: 'GET',
         path: '/',
@@ -34,6 +43,18 @@ const init = async () => {
                     version,
                 },
             };
+        },
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/app/{path*}',
+        handler: {
+            directory: {
+                path: path.join(__dirname, '../client/build/'),
+                listing: false,
+                index: true,
+            },
         },
     });
 
